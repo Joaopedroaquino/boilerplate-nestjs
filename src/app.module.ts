674e955de09f9b73d37f3dd3fe0c67b3492409b8
joaopedroaquino <aqerhack@gmail.com>
2022-11-30 10:44:39 -0300

    init: :whale: init first modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { ProfileModule } from './modules/profile/profile.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, ProfileModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
